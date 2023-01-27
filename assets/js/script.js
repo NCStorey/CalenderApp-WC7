ac650eca56018e640546e0aabedc18758260636d
NCStorey <ncsansom@hotmail.co.uk>
2023-01-27 14:53:05 +0000

    framework of page added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,31 @@ let timeBlockCont = $('#timeBlockCont')
 let currentTime = moment().format("dddd, Do MMMM YYYY");
 let currentTimeP = $('#currentDay')
 
+
 console.log(currentTime)
 currentTimeP.text(currentTime)
 
 for (let i = 8; i <= 17; i++){
-    let Hourbar = $('<span>');
-    Hourbar.text('hourbar' + [i])
-    Hourbar.attr('data-marker', [i])
-    Hourbar.addClass('class', 'time-block')
-    timeBlockCont.append(Hourbar)
+
+    let hourBar = $('<span>');
+    hourBar.attr('data-marker', [i]);
+    hourBar.addClass('row');
+    timeBlockCont.append(hourBar);
+
+    let hourTab = $('<span>');
+    hourTab.text([i] + ':00');
+    hourTab.addClass('col-1 hour');
+    hourBar.append(hourTab);
+
+    let hourBlock = $('<span>');
+    hourBlock.text('hourbar' + [i]);
+    hourBlock.addClass('col-10 time-block');
+    hourBar.append(hourBlock)
+
+    let saveBlock = $('<span>');
+    saveBlock.addClass('col-1 saveBtn');
+    saveBlock.text('save')
+    hourBar.append(saveBlock);
+
 }
+
